5ec369b0e093feafe3682784410c679ebde0dc6b
Calvin Rai <raicalvin@gmail.com>
2018-09-26 15:43:13 -0700

    Updated filter input field to have proper positioning and border-radius

--- app/src/App.js
+++ app/src/App.js
@@ -5,23 +5,37 @@ class App extends Component {
   render() {
     return (
       <div id="app">
-        <div id="header">
+        <div id="header" className="flex-container flex-container-center">
           <span>KYOTO EXPLORE</span>
         </div>
         <div id="content">
           <div id="sidebar">
-            <div className="blank-item" />
-            <div className="blank-item" />
-            <div className="blank-item" />
-            <div className="blank-item" />
-            <div className="blank-item" />
-            <div className="blank-item" />
-            <div className="blank-item" />
-            <div className="blank-item" />
+            <div id="input-field">
+              <input type="text" name="fname" className="input-field-filter" />
+            </div>
+            <div id="sidebar-list">
+              <ul>
+                <li>
+                  <div className="blank-item" />
+                </li>
+                <li>
+                  <div className="blank-item" />
+                </li>
+                <li>
+                  <div className="blank-item" />
+                </li>
+                <li>
+                  <div className="blank-item" />
+                </li>
+                <li>
+                  <div className="blank-item" />
+                </li>
+              </ul>
+            </div>
           </div>
           <div id="map" />
         </div>
-        <div id="footer">
+        <div id="footer" className="flex-container flex-container-center">
           <span>Made with LOVE. And React. By Calvin.</span>
         </div>
       </div>
