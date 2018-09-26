4debd196e25f331427ed8d17e3b198f214049614
Calvin Rai <raicalvin@gmail.com>
2018-09-26 16:12:46 -0700

    Updated list of places with proper placeholder titles

--- app/src/App.js
+++ app/src/App.js
@@ -19,19 +19,73 @@ class App extends Component {
             <div id="sidebar-list">
               <ul>
                 <li>
-                  <div className="blank-item" />
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Kyoto Palace</span>
+                  </div>
                 </li>
                 <li>
-                  <div className="blank-item" />
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Fushimi Inari-taisha</span>
+                  </div>
                 </li>
                 <li>
-                  <div className="blank-item" />
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Arashiyama</span>
+                  </div>
                 </li>
                 <li>
-                  <div className="blank-item" />
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Gion</span>
+                  </div>
                 </li>
                 <li>
-                  <div className="blank-item" />
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Nijo Castle</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Kifune Shrine</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">
+                      Kyoto Imperial Palace
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Nishiki Market</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Tō-ji</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Kyoto Tower</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Philosopher's Walk</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">Maruyama Park</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="list-item flex-container-list-item">
+                    <span className="list-item-span">
+                      Kyoto International Manga Museum
+                    </span>
+                  </div>
                 </li>
               </ul>
             </div>
