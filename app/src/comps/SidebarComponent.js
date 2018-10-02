00f4f48c5e7afa434488c243d4a25db940e65b8e
Calvin Rai <raicalvin@gmail.com>
2018-10-02 13:30:24 -0700

    Removed input field component and replaced with JSX

--- app/src/comps/SidebarComponent.js
+++ app/src/comps/SidebarComponent.js
@@ -3,10 +3,28 @@ import Input from "./InputComponent";
 import ListItem from "./ListItemComponent";
 
 class Sidebar extends Component {
+  state = {
+    query: ""
+  };
+
+  updateQuery = query => {
+    this.setState({ query: query.trim() });
+    console.log("something changed");
+  };
+
   render() {
     return (
       <div id="sidebar">
-        <Input />
+        <div id="input-field" className="flex-container flex-container-center">
+          <input
+            type="text"
+            name="fname"
+            placeholder="Filter results..."
+            className="input-field-filter"
+            value={this.state.query}
+            onChange={e => this.updateQuery(e.target.value)}
+          />
+        </div>
         <div id="sidebar-list">
           <ul>
             {this.props.places.map(place => (
