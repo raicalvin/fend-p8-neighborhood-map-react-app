6b666ef6383fe9c86576ce86d958043a17a3b7e6
Calvin Rai <raicalvin@gmail.com>
2018-10-05 15:49:10 -0700

    Added functional code for beginServiceWorker.js file

---
+++ app/src/beginServiceWorker.js
@@ -0,0 +1,12 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("/sw.js", { scope: "/" }).then(
+      function(reg) {
+        console.log("ServiceWorker registered successfully.");
+      },
+      function(err) {
+        console.log("ServiceWorker failed registration.");
+      }
+    );
+  });
+}
